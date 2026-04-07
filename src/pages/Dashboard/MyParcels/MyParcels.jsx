c9913f8207e7d0fac2779f3b91ad7099045063db
potla <shahriarhimel00@gmail.com>
2026-04-07 14:27:26 +0600

    tanstack queryy has been used for parcel's data fetching

--- src/pages/Dashboard/MyParcels/MyParcels.jsx
+++ src/pages/Dashboard/MyParcels/MyParcels.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import useAxiosSecurity from '../../../hooks/useAxiosSecurity'
+import UseAuth from '../../../hooks/UseAuth'
+import { useQuery } from '@tanstack/react-query'
 
 const MyParcels = () => {
+
+    const axios = useAxiosSecurity()
+    const { user } = UseAuth()
+
+    const { data : parcell = [] } = useQuery({
+        queryKey: ['myParcels', user?.email],
+        queryFn: async () => {
+            const res = await axios.get(`/parcels?email=${user.email}`)
+            return res.data
+        }
+    })
+
+    console.log(parcell)
+
   return (
-    <div>MyParcels</div>
+    <div>MyParcels : {parcell.length} </div>
   )
 }
 
